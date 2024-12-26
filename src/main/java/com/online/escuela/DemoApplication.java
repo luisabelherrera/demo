ee3c9f4ad0779ef80f4b868f0b031d8d36798b88
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2024-12-25 22:05:19 -0500

    feat: Actualización de configuraciones para PostgreSQL, Redis y MongoDB
    - Configuración de `@EnableJpaRepositories` con basePackages actualizado.
    - Se añadió el nombre de la aplicación (`spring.application.name=demo`).
    - Ajustes en la conexión a PostgreSQL (URL, credenciales y propiedades de Hibernate).
    - Configuración de Redis como proveedor de caché.
    - Adición de configuraciones para la conexión a MongoDB.
    - Habilitación de multipart para carga de archivos con límites ajustados.

--- src/main/java/com/online/escuela/DemoApplication.java
+++ src/main/java/com/online/escuela/DemoApplication.java
@@ -2,8 +2,13 @@ package com.online.escuela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
+
+
+@EnableJpaRepositories(basePackages = "com.online.escuela.repositories.jpa")
+//opcional  EntityScan
 @SpringBootApplication
 public class DemoApplication {
 
