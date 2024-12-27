bd00a91979768b63c3c1ee3f7d9a63ed25b227b4
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2024-12-27 17:59:23 -0500

    Cambios en las entidades   (1)

--- src/main/java/com/online/escuela/model/entityLogin/UserEntity.java
+++ src/main/java/com/online/escuela/model/entityLogin/UserEntity.java
@@ -1,4 +1,41 @@
 package com.online.escuela.model.entityLogin;
 
+
+import java.util.Set;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_rol", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_rol"))
+    private Set<Rol> roles;
+
+
+
+
+    public UserEntity(Long id, String username, String password, String email, Set<Rol> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public UserEntity() {
+    }
+
 }
--- src/main/java/com/online/escuela/model/entityLogin/dto/UserDto.java
+++ src/main/java/com/online/escuela/model/entityLogin/dto/UserDto.java
@@ -1,4 +1,76 @@
 package com.online.escuela.model.entityLogin.dto;
 
+import java.util.Set;
+
+import com.online.escuela.model.entityLogin.Rol;
+
+
 public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+    private Set<Rol> roles;
+    private String password;  
+
+    public UserDto() {
+    }
+
+    public UserDto(Long id, String username, String email, Set<Rol> roles, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Rol> roles) {
+        this.roles = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", roles=" + roles +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
