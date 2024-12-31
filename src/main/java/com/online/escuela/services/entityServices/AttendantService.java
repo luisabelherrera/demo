3eeb5681d658f68c30d15c85ddb41284e39cf6e0
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2024-12-31 18:59:14 -0500

    Cambios en el interface service (1)

--- src/main/java/com/online/escuela/services/entityServices/AttendantService.java
+++ src/main/java/com/online/escuela/services/entityServices/AttendantService.java
@@ -1,4 +1,17 @@
 package com.online.escuela.services.entityServices;
 
-public class AttendantService {
+import java.util.List;
+import java.util.Optional;
+
+import com.online.escuela.model.entity.dto.AttendantDTO;
+
+public interface AttendantService {
+
+    List<AttendantDTO> findAll();
+
+    Optional<AttendantDTO> findById(long id);
+
+    AttendantDTO save(AttendantDTO attendatDTO);
+
+    void deleteById(long id);
 }
