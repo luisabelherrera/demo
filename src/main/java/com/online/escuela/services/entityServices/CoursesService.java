a239f61372d5c6ec0ab2f77fbaaa49d91568cbfd
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2025-01-01 18:43:23 -0500

    Cambios en las entidades (5)

--- src/main/java/com/online/escuela/services/entityServices/CoursesService.java
+++ src/main/java/com/online/escuela/services/entityServices/CoursesService.java
@@ -1,4 +1,15 @@
 package com.online.escuela.services.entityServices;
 
-public class CoursesService {
+import java.util.List;
+import java.util.Optional;
+
+import com.online.escuela.model.entity.dto.CoursesDTO;
+
+public interface CoursesService {
+
+    List<CoursesDTO> findAll();
+  Optional<CoursesDTO> findById(long id);
+  CoursesDTO save(CoursesDTO  coursesDTO);
+  void deleteById(long id);
+
 }
