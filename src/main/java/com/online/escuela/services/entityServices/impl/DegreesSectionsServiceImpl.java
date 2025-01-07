239cf04d0eb5da959144e0f28879a35f156e3d50
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2025-01-06 22:00:55 -0500

    Cambios en el Impl (4)

--- src/main/java/com/online/escuela/services/entityServices/impl/DegreesSectionsServiceImpl.java
+++ src/main/java/com/online/escuela/services/entityServices/impl/DegreesSectionsServiceImpl.java
@@ -1,4 +1,43 @@
 package com.online.escuela.services.entityServices.impl;
 
-public class DegreesSectionsServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.online.escuela.model.entity.dto.DegreesSectionsDTO;
+import com.online.escuela.repositories.jpa.DegreesSectionsRepositories;
+import com.online.escuela.services.entityServices.DegreesSectionsService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DegreesSectionsServiceImpl  implements DegreesSectionsService {
+    
+    private final DegreesSectionsRepositories degreesSectionsRepositories;
+    
+    @Override
+    public List<DegreesSectionsDTO> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<DegreesSectionsDTO> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public DegreesSectionsDTO save(DegreesSectionsDTO degreesSectionsDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void deleteById(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
 }
