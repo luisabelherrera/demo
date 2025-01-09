ed8fb5a51a901eae88a788a6fe616602b46e371b
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2025-01-09 15:11:15 -0500

    Cambios en el Impl (6)

--- src/main/java/com/online/escuela/services/entityServices/impl/LevelDetailServiceImpl.java
+++ src/main/java/com/online/escuela/services/entityServices/impl/LevelDetailServiceImpl.java
@@ -1,4 +1,37 @@
 package com.online.escuela.services.entityServices.impl;
 
-public class LevelDetailServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.online.escuela.model.entity.dto.LevelDetailDTO;
+import com.online.escuela.services.entityServices.LevelDetailService;
+
+@Service
+public class LevelDetailServiceImpl implements  LevelDetailService{
+
+    @Override
+    public List<LevelDetailDTO> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<LevelDetailDTO> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public LevelDetailDTO save(LevelDetailDTO levelDetailDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
 }
--- src/main/java/com/online/escuela/services/entityServices/impl/LevelsServicesImpl.java
+++ src/main/java/com/online/escuela/services/entityServices/impl/LevelsServicesImpl.java
@@ -1,4 +1,37 @@
 package com.online.escuela.services.entityServices.impl;
 
-public class LevelsServicesImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.online.escuela.model.entity.dto.LevelsDTO;
+import com.online.escuela.services.entityServices.LevelsServices;
+
+@Service
+public class LevelsServicesImpl implements LevelsServices{
+
+    @Override
+    public List<LevelsDTO> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<LevelsDTO> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public LevelsDTO save(LevelsDTO levelsDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
 }
