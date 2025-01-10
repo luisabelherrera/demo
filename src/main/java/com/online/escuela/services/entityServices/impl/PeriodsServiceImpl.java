cc7050f98751d297d45ba3439b9806b3170f9286
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2025-01-10 18:22:20 -0500

    Cambios en el Impl (7)

--- src/main/java/com/online/escuela/services/entityServices/impl/PeriodsServiceImpl.java
+++ src/main/java/com/online/escuela/services/entityServices/impl/PeriodsServiceImpl.java
@@ -1,4 +1,37 @@
 package com.online.escuela.services.entityServices.impl;
 
-public class PeriodsServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.online.escuela.model.entity.dto.PeriodsDTO;
+import com.online.escuela.services.entityServices.PeriodsService;
+
+@Service
+public class PeriodsServiceImpl implements PeriodsService {
+
+    @Override
+    public List<PeriodsDTO> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public PeriodsDTO save(PeriodsDTO periodsDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public Optional<PeriodsDTO> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
 }
