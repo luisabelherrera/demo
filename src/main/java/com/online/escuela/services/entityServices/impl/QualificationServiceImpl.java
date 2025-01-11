2ffac4ffab6ecba9d0b0efeaab4dabe17849bd27
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2025-01-11 18:51:06 -0500

    Cambios en el Impl (8)

--- src/main/java/com/online/escuela/services/entityServices/impl/QualificationServiceImpl.java
+++ src/main/java/com/online/escuela/services/entityServices/impl/QualificationServiceImpl.java
@@ -1,4 +1,37 @@
 package com.online.escuela.services.entityServices.impl;
 
-public class QualificationServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.online.escuela.model.entity.dto.QualificationDTO;
+import com.online.escuela.services.entityServices.QualificationService;
+
+@Service
+public class QualificationServiceImpl implements QualificationService {
+
+    @Override
+    public List<QualificationDTO> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<QualificationDTO> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public QualificationDTO save(QualificationDTO qualificationDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
 }
